cb299c316a7b5ce85771e1d053fc8c450d44a677
zaccarias <zaccariah23@gmail.com>
2022-03-19 15:01:24 +0800

    Pog Champ

---
+++ app/src/main/java/com/example/group_decisionbasedgame/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.group_decisionbasedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        ProgressBar progressBar =findViewById(R.id.loading);
+        TextView textView =findViewById(R.id.press);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Please wait");
+                progressBar.setVisibility(View.VISIBLE);
+
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        textView.setText("Loading Complete");
+                        progressBar.setVisibility(View.INVISIBLE);
+                        textView.setTextColor(Color.parseColor("#FF03DAC5"));
+                        finish();
+                        startActivity(new Intent(getApplicationContext(),uidesign.class));
+
+                    }
+                },4000);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/group_decisionbasedgame/uidesign.java
@@ -0,0 +1,34 @@
+package com.example.group_decisionbasedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class uidesign extends AppCompatActivity {
+    private Button button,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uidesign);
+        button=findViewById(R.id.Next);
+        button2=findViewById(R.id.Leave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(uidesign.this,Play.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(uidesign.this,Quit.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
