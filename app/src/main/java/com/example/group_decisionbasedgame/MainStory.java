e3832dd122a3d08252723a8b59b525ddf60957b3
zaccarias <zaccariah23@gmail.com>
2022-03-19 15:11:35 +0800

    Added new code

---
+++ app/src/main/java/com/example/group_decisionbasedgame/MainStory.java
@@ -0,0 +1,14 @@
+package com.example.group_decisionbasedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainStory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_story);
+    }
+}
