48cf8f1d0bcfdab41b144e2c70dfdd46be136452
zaccarias <zaccariah23@gmail.com>
2022-03-21 10:57:58 +0800

    Added new code

--- app/src/main/java/com/example/group_decisionbasedgame/MainStory.java
+++ app/src/main/java/com/example/group_decisionbasedgame/MainStory.java
@@ -3,16 +3,6 @@ package com.example.group_decisionbasedgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Random;
-
 public class MainStory extends AppCompatActivity {
 
     @Override
